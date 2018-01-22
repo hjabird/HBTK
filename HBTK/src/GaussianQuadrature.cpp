9ca288b4fcdbf488032f9b89c500dbf956d13473
HJA Bird <9040797+hjabird@users.noreply.github.com>
2018-01-22 09:37:37 +0000

    Corrections. Terse static asserts given messages.

--- HBTK/src/GaussianQuadrature.cpp
+++ HBTK/src/GaussianQuadrature.cpp
@@ -214,13 +214,13 @@ double HBTK::jacobi_integral(double alpha, double beta)
 		assert(alpha > -1);
 		if (alpha == -0.5) { result = HBTK::Constants::pi(); }
 		else {
-			int n = 2 * alpha;
+			int n = (int)(2 * alpha); // Should be an int anyway.
 			result = HBTK::Constants::pi() * fact(n) / 
 				(fact((n + 1) / 2) * fact((n - 1) / 2) * pow(2, n));
 		}
 	}
 	else if (alpha == -beta) {
-		if (abs(alpha) == 0.5) { result == HBTK::Constants::pi(); }
+		if (abs(alpha) == 0.5) { result = HBTK::Constants::pi(); }
 		else { assert(false); }
 	}
 	else { assert(false); } // Perhaps numerically integrate?
