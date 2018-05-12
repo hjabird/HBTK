4538aa5383bf70166a82ec9ef803106a112a33fb
hjabird <hugh.bird.1@research.glasgow.ac.uk>
2018-05-12 23:58:05 +0100

    Removed stdafx, targetver.

--- HBTK/src/FortranSequentialInputStream.cpp
+++ HBTK/src/FortranSequentialInputStream.cpp
@@ -1,4 +1,3 @@
-#include "stdafx.h"
 #include "FortranSequentialInputStream.h"
 /*////////////////////////////////////////////////////////////////////////////
 FortranSequentialInputStream.cpp
--- HBTK/src/FortranSequentialOutputStream.cpp
+++ HBTK/src/FortranSequentialOutputStream.cpp
@@ -1,4 +1,3 @@
-#include "stdafx.h"
 #include "FortranSequentialOutputStream.h"
 /*////////////////////////////////////////////////////////////////////////////
 FortranSequentialInputStream.cpp
